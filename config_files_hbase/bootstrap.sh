a50dedb2577181e4683ee52af704f4d9c98735ef
abdul.rafay <rafayjangsher2@gmail.com>
2023-01-29 14:16:24 +0100

    hbase config files added

---
+++ config_files_hbase/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+: ${HADOOP_PREFIX:=/usr/local/hadoop}
+
+$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
+
+rm /tmp/*.pid
+
+# installing libraries if any - (resource urls added comma separated to the ACP system variable)
+cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+
+# replace config files if provided
+cp /mnt/hadoop-config/* $HADOOP_PREFIX/etc/hadoop/
+
+# start sshd
+/usr/sbin/sshd
+
+if [[ $1 == "-d" ]]; then
+  while true; do sleep 1000; done
+fi
+
+if [[ $1 == "-bash" ]]; then
+  /bin/bash
+fi
